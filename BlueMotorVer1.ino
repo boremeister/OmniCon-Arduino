98ba3d1dd94f48a80fdbd586e29821a2d25975f6
boremeister <boremeister@gmail.com>
2014-12-14 00:29:26 +0100

    execute command methods

--- BlueMotorVer1.ino
+++ BlueMotorVer1.ino
@@ -26,6 +26,8 @@ SoftwareSerial BTSerial(RX_PIN,TX_PIN);	// atmega RX / TX
 String response;
 String startString = "~", endString = "#";
 char endChar = '#';
+int const MAX_SERVO_ANGLE = 180;
+int const MAX_DC_SPEED = 100;
 
 // command array
 char command[7];
@@ -86,7 +88,7 @@ void loop()
 			BTSerial.println(startString + response + endString);
 
 			// EXECUTE COMMAND
-			executeCommand(command);
+			executeCommandWithCheck(command);
 		}
 	}
 
@@ -96,6 +98,15 @@ void loop()
 }
 
 // OTHER METHODS
+
+void executeCommandWithCheck(char* c){
+	
+	// check that command length is correct
+	size_t len = strlen(c);
+	if (len > 1 && len < 7){
+		executeCommand(c);
+	}
+}
 
 void executeCommand(char* c){
 
@@ -107,6 +118,49 @@ void executeCommand(char* c){
 		turnLedOFF(LED_GREEN);
 	}
 
+	/*
+	
+	command list
+
+	servo 1 - 00 1(smer vrtenja) XYZ (kot ~ 0-180)
+	servo 2 - 01 1(smer vrtenja) XYZ (kot ~ 0-180)
+	DC 1 - 10 1/0 (smer vrtenja ~ 1 - naprej, 0 - nazaj) XYZ (hitrost ~ 0-100)
+	DC 2 - 11 1/0 (smer vrtenja ~ 1 - naprej, 0 - nazaj) XYZ (hitrost ~ 0-100)
+	
+	*/
+
+	char motorType = c[0];	// motor type
+	char motorNumber = c[1];	// motor number
+	char direction = c[2];	// drive direction
+	
+	/*
+	* NOT TESTED YET
+	*/
+	char* val;
+	strncpy(val, c + 3, 3);
+	val[4] = '\0';
+	int angleOrSpeed = atoi(val);
+
+	if (motorType == '0'){
+		
+		/*
+		* SERVO motor
+		*/
+
+		setServoAngle(motorNumber, angleOrSpeed);
+
+	}
+	else if (motorType == '1'){
+		
+		/*
+		* DC motor
+		*/
+
+		setDcSpeed(motorNumber, direction, angleOrSpeed);
+
+	}
+
+
 }
 
 void turnLedON(int pin){
@@ -116,3 +170,35 @@ void turnLedON(int pin){
 void turnLedOFF(int pin){
 	digitalWrite(pin, LOW);
 }
+
+void setServoAngle(char motorNum, int val){
+	// set servo angle to specific value
+
+	// check val
+	if (val > MAX_SERVO_ANGLE)
+		val = MAX_SERVO_ANGLE;
+
+	if (motorNum == '0'){
+		// servo 1
+	}
+	else if (motorNum = '1'){
+		// servo 2
+	}
+
+}
+
+void setDcSpeed(char motorNum, char direction, int val){
+	// set DC speed and direction
+
+	// check val
+	if (val > MAX_DC_SPEED)
+		val = MAX_DC_SPEED;
+
+	if (motorNum == '0'){
+		// DC 1
+	}
+	else if (motorNum = '1'){
+		// DC 2
+	}
+}
+
