17225facc541ae54ef94e780f2503fb99dc43b05
boremeister <boremeister@gmail.com>
2014-12-14 21:37:02 +0100

    servo 1 - turning, first test

--- BlueMotorVer1.ino
+++ BlueMotorVer1.ino
@@ -8,7 +8,7 @@
 
 */
 
-//#include <Servo.h>
+#include <Servo.h>
 #include <SoftwareSerial.h>		// http://arduino.cc/en/Reference/SoftwareSerial
 
 // -------------------------- prepare --------------------------
@@ -17,10 +17,15 @@
 #define RX_PIN 7						// Bluetooth TX pin (HC-05 TX)
 #define TX_PIN 8						// Bluetooth RX pin (HC-05 RX)
 #define LED_GREEN 13					// LED Green
-#define SERVO_ONE 12					// Servo one
+#define SERVO_1 9						// Servo 1
+#define SERVO_2 10						// Servo 2
 
 // SoftwareSerial - BlueTooth HC-05
 SoftwareSerial BTSerial(RX_PIN,TX_PIN);	// atmega RX / TX
+
+// Servos
+Servo servo1;
+//Servo servo2;
 
 // other variables
 String response;
@@ -28,6 +33,8 @@ String startString = "~", endString = "#";
 char endChar = '#';
 int const MAX_SERVO_ANGLE = 180;
 int const MAX_DC_SPEED = 100;
+int servo1pos = 0;
+int servo2pos = 0;
 
 // command array
 char command[7];
@@ -45,6 +52,13 @@ void setup()
 
 	// leds
 	pinMode(LED_GREEN, OUTPUT);
+
+	// servos
+	servo1.attach(SERVO_1);
+	//servo2.attach(SERVO_2);
+
+	// set servos to default position
+	//servoTest(servo1);
 
 	// default start delay
 	delay(1000);
@@ -93,7 +107,7 @@ void loop()
 	}
 
 	// delay for loop
-	delay(1000);
+	//delay(1000);
 
 }
 
@@ -122,44 +136,71 @@ void executeCommand(char* c){
 	
 	command list
 
-	servo 1 - 00 1(smer vrtenja) XYZ (kot ~ 0-180)
-	servo 2 - 01 1(smer vrtenja) XYZ (kot ~ 0-180)
-	DC 1 - 10 1/0 (smer vrtenja ~ 1 - naprej, 0 - nazaj) XYZ (hitrost ~ 0-100)
-	DC 2 - 11 1/0 (smer vrtenja ~ 1 - naprej, 0 - nazaj) XYZ (hitrost ~ 0-100)
+	servo 1 - 0 1(smer vrtenja) XYZ (kot ~ 0-180)
+	servo 2 - 1 1(smer vrtenja) XYZ (kot ~ 0-180)
+	DC 1 - 2 1/0 (smer vrtenja ~ 1 - naprej, 0 - nazaj) XYZ (hitrost ~ 0-100)
+	DC 2 - 3 1/0 (smer vrtenja ~ 1 - naprej, 0 - nazaj) XYZ (hitrost ~ 0-100)
 	
 	*/
 
-	char motorType = c[0];	// motor type
-	char motorNumber = c[1];	// motor number
-	char direction = c[2];	// drive direction
+	char motor = c[0];	// motor type
+	char direction = c[1];	// drive direction
 	
 	/*
-	* NOT TESTED YET
+	* get angle/speed
+	*
+	* The character encodings for digits in AscII are all in order from 48 (for '0') to 57 (for '9').
+	* The integer value for any digit is the digit less 48 ('0' is 48).
 	*/
-	char* val;
-	strncpy(val, c + 3, 3);
-	val[4] = '\0';
-	int angleOrSpeed = atoi(val);
+	int enka = c[2] - '0';
+	int dvojka = c[3] - '0';
+	int trojka = c[4] - '0';
+	int angleOrSpeed = enka * 100 + dvojka * 10 + trojka;
 
-	if (motorType == '0'){
-		
-		/*
-		* SERVO motor
-		*/
+	switch (motor){
+		case '0':
 
-		setServoAngle(motorNumber, angleOrSpeed);
+			/*
+			* SERVO motor 1
+			*/
 
+			setServoAngle(motor, angleOrSpeed);
+
+			break;
+
+		case '1':
+
+			/*
+			* SERVO motor 2
+			*/
+
+			setServoAngle(motor, angleOrSpeed);
+
+			break;
+
+		case '2':
+
+			/*
+			* DC motor 1
+			*/
+
+			setDcSpeed(motor, direction, angleOrSpeed);
+
+			break;
+
+		case '3':
+
+			/*
+			* DC motor 2
+			*/
+
+			setDcSpeed(motor, direction, angleOrSpeed);
+
+			break;
+
+		default:
+			break;
 	}
-	else if (motorType == '1'){
-		
-		/*
-		* DC motor
-		*/
-
-		setDcSpeed(motorNumber, direction, angleOrSpeed);
-
-	}
-
 
 }
 
@@ -180,9 +221,12 @@ void setServoAngle(char motorNum, int val){
 
 	if (motorNum == '0'){
 		// servo 1
+		servo1.write(val);
+		delay(10);
 	}
 	else if (motorNum = '1'){
 		// servo 2
+		//servo2.write(val);
 	}
 
 }
@@ -199,6 +243,23 @@ void setDcSpeed(char motorNum, char direction, int val){
 	}
 	else if (motorNum = '1'){
 		// DC 2
+	}
+}
+
+/*
+* servo motor test - sweep (http://arduino.cc/en/Tutorial/sweep)
+*/
+void servoTest(Servo servo){
+	int pos;
+	for (pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+	{                                  // in steps of 1 degree 
+		servo.write(pos);              // tell servo to go to position in variable 'pos' 
+		delay(15);                       // waits 15ms for the servo to reach the position 
+	}
+	for (pos = 180; pos >= 1; pos -= 1)     // goes from 180 degrees to 0 degrees 
+	{
+		servo.write(pos);              // tell servo to go to position in variable 'pos' 
+		delay(15);                       // waits 15ms for the servo to reach the position 
 	}
 }
 
